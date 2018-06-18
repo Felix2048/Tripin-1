2ae4ebe0e18fd4387875e250e38e1798fe59865a
Felix2048 <infinitesamsarax@outlook.com>
2018-06-19 05:35:29 +0800

    添加ItemTouchHelper

--- app/src/main/java/com/android/tripin/fragment/schedule/ScheduleFragment.java
+++ app/src/main/java/com/android/tripin/fragment/schedule/ScheduleFragment.java
@@ -3,6 +3,7 @@ package com.android.tripin.fragment.schedule;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import com.android.tripin.base.BaseFragment;
 import com.android.tripin.entity.Pin;
 import com.android.tripin.manager.DataManager;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -41,12 +43,43 @@ public class ScheduleFragment extends BaseFragment {
         View view = inflater.inflate(R.layout.fragment_schedule,container,false);
         pinList = DataManager.getPlanMapDiagramHashMap().get(DataManager.getCurrentPlan()).getPinList();
         recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
         scheduleRecycleViewAdapter = new ScheduleRecycleViewAdapter(pinList);
         recyclerView.setAdapter(scheduleRecycleViewAdapter);
+        //为RecycleView绑定触摸事件
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.Callback() {
+            @Override
+            public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                //首先回调的方法 返回int表示是否监听该方向
+                int dragFlags = ItemTouchHelper.UP|ItemTouchHelper.DOWN;//拖拽
+                int swipeFlags = ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT;//侧滑删除
+                return makeMovementFlags(dragFlags,swipeFlags);
+            }
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                //滑动事件
+                Collections.swap(pinList,viewHolder.getAdapterPosition(),target.getAdapterPosition());
+                scheduleRecycleViewAdapter.notifyItemMoved(viewHolder.getAdapterPosition(),target.getAdapterPosition());
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                //侧滑事件
+                pinList.remove(viewHolder.getAdapterPosition());
+                scheduleRecycleViewAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+            }
+
+            @Override
+            public boolean isLongPressDragEnabled() {
+                //是否可拖拽
+                return true;
+            }
+        });
+        helper.attachToRecyclerView(recyclerView);
         return view;
     }
 
-    public ScheduleFragment() {
-    }
 }
